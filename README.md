1a97e63600f027e08b06a095980e0b82c6b52d52
tablonimus <lautamartin1995@gmail.com>
2023-02-20 21:19:39 -0300

    new

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# DesafioN1
+# Bienvenido al Desafio sobre JavaScript de DEV.F.
+
+¡Por favor lee con atención!
+
+Hay cosas puntuales que tal vez no vimos específicamente en el material de las clases, vas a tener que investigar un poquito por tu cuenta (el buscador de Google es muy útil en estos casos).
+
+### 1. FORKEAR REPOSITORIO EN TU CUENTA
+
+Primero debes forkear este repo, haciendo click en el botón `fork` de arriba a la derecha en la plataforma de Git Hub.
+
+Una vez que tengas una copia de este repo en tu cuenta de `github`, cloná el repo dentro de tu pc(debes clonarlo desde TU PROPIA cuenta de GitHub.). Una vez clonado entrá a esa carpeta y ejecutá los siguientes comandos desde la consola bash:
+
+    npm install
+    npm test
+
+> En un principio todos los test estarán en rojo, es tu tarea solucionarlos y que pasen a ser correctos.
+
+### 2. RESOLVER EL CHALLENGE
+
+Tu tarea es completar el código en `desafio.js` de tal forma que pasen la mayoría de los tests.
+NO DEBES TOCAR OTRO ARCHIVO QUE NO SEA "desafio.js"
+
+### 3. ENTREGA
+
+Correr por ultima vez los tests y verificar cuantos pasan. Ten en cuenta que si te aparece "1 failed;1 total" es porque tienes un error de sintaxis: seguramente falta o sobra una llave, paréntesis, punto y coma, etc.
+
+Saca un print de pantalla de tus tests.( Envia a tus senseis la captura de pantalla con el resultado de los test via slack.)
+
+Luego, debes subir un commit a tu repo. Para hacerlo, debes ejecutar el siguiente comando:
+
+    git add .
+    git commit -m 'desafio superado commit'
+    git push origin main
+
+Una vez finalizado, chequea que veas los cambios reflejados en el repo de tu cuenta de github (entrando a tu repo desde el browser.)
+
+### GUIA DE ERRORES COMUNES
+
+Para identificar el error, vas a tener que leerlo en la consola.
+
+- "jest" no se reconoce como un comando externo o interno...:
+
+  1. Borrar la carpeta `node_modules` y el archivo `package-lock.json` e instalar nuevamente ( `npm install` ).
+  2. Si esto no funciona, instalar test con el comando `npm install jest`.
+
+- 1 failed, 1 total:
+
+  1. Tenes un error de sintaxis. Revisa el último ejercicio que hayas hecho, seguramente falta o sobra una llave, paréntesis, punto y coma, etc.
+
+- Author identity unknown.
+
+  1. Intenta ejecutar los siguientes comandos para configurar tu cuenta:
+
+     - git config --global user.name "Tu usuario de GitHub aca"
+     - git config --global user.email "Tu email aca"
+
+  2. Ingresa a [Github](https://docs.github.com/es/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) y sigue las instrucciones para configurar tu token.
+
+- La consola se tilda en `Runs`:
+  1. Revisa tu código, tenes un bucle infinito. Tenes que checkear la condición de corte de tus bucles.
---
+++ desafio.js
@@ -0,0 +1,272 @@
+/* 
+ A tener en cuenta:
+       - No debes cambiar los nombres de las funciones.
+       - No debes usar console.log().
+       - DEBES usar return para que la funcion retorne lo pedido.
+       - Tu código debe estar siempre encerrado entre las {} de cada función(ejercicio).
+
+*/
+
+function sonIguales(elemento1, elemento2) {
+  // Devuelve "true" si "elemento1" y "elemento2" son estrictamente iguales
+  // De lo contrario, devuelve "false"
+  //↓↓↓ Tu código: ↓↓↓ */
+
+
+
+}
+
+function tienenMismaLongitud(str1, str2) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+}
+
+function esPar(num) {
+  // Devuelve "true" si "num" es par
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+}
+
+function esImpar(num) {
+  // Devuelve "true" si "num" es impar
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+}
+
+function elevarAlCuadrado(num) {
+  // Devuelve el valor de "num" elevado al cuadrado
+  // ojo: No es raiz cuadrada!
+  // Tu código:
+
+}
+
+function elevarAlCubo(num) {
+  // Devuelve el valor de "num" elevado al cubo
+  // Tu código:
+
+}
+
+function elevar(num, exponent) {
+  // Devuelve el valor de "num" elevado al exponente dado en "exponent"
+  // Tu código:
+
+}
+
+function esPositivo(numero) {
+  //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
+  //Si el número es positivo, devolver ---> "Este numero es positivo"
+  //Si el número es negativo, devolver ---> "Este numero es negativo"
+  //Si el número es 0, devuelve false
+
+}
+
+function combinarNombres(nombre, apellido) {
+  // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
+  // Ejemplo: "Lionel", "Messi" -> "Lionel Messi"
+  // Tu código:
+
+}
+
+function obtenerSaludo(nombre) {
+  // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
+  // "Exequiel" -> "Hola Exequiel!"
+  // Tu código:
+
+}
+
+function agregarSimboloExclamacion(str) {
+  // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
+  // Ejemplo: "hello world" pasaría a ser "hello world!"
+  // Tu código:
+
+}
+
+function deEuroAdolar(euro) {
+  /*  Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
+  como parámetro un número de euros y calcule el cambio en dólares. "suponiendo que vale 1.20"
+  Escribe tu código aquí */
+
+}
+
+function esVocal(letra) {
+  /*   Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+  Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle
+  que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
+  Si no es vocal, tambien debe devolver "Dato incorrecto".
+  Escribe tu código aquí */
+
+}
+
+function esDiezOCinco(numero) {
+  // Devuelve "true" si "numero" es 10 o 5
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+}
+
+function estaEnRango(numero) {
+  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+}
+
+function operadoresLogicos(num1, num2, num3) {
+  //La función recibe tres números distintos.
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+
+}
+
+function tablaDelSeis() {
+  //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+  //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+  //resultado esperado: [0 , 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]
+  //Escribe tu código aquí:
+
+}
+
+function tieneTresDigitos(numero) {
+  //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+  //PISTA: deberas usar un metodo para pasar el numero a string y asi poder leer su longitud con .length ;)
+  //Escribe tu código aquí:
+
+}
+
+
+function obtenerLargoDelArray(array) {
+  // Devuelve el largo de un array
+  // Tu código:
+ 
+}
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+  
+}
+
+function devolverPrimerElemento(array) {
+  /*   Devuelve el primer elemento de un  array (pasado por parametro)
+   ↓↓↓ Tu código: ↓↓↓ */
+
+}
+
+function devolverUltimoElemento(array) {
+  // Devuelve el último elemento de un array
+  // Tu código:
+
+}
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello','world!'] -> 'Hello World!'
+  // Tu código:
+
+}
+
+function arrayContiene(array, elemento) {
+  // Comprueba si el elemento existe dentro de "array"
+  // Devuelve "true" si está, o "false" si no está
+  // Tu código:
+
+}
+
+function agregarNumeros(numeros) {
+  // "numeros" es un array de enteros (int/integers)
+  // Suma todos los enteros y devuelve el valor(puedes usar .reduce() )
+  // Tu código:
+
+}
+
+function cuentoElementos(arreglo) {
+  //Realiza una función que retorne la cantidad de los elementos del array donde el valor es mayor a 18.
+  //Escribe tu código aquí:
+
+}
+
+function empiezaConNueve(n) {
+  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
+  //inicia con 9 y false en otro caso.
+  //PISTA: para saber si un numero comienza con 9 debes pasarlo a string y analizar la posicion [0] del numero convertido.
+  //Escribe tu código aquí
+
+}
+
+function todosIguales(arreglo) {
+  //indique si todos los elementos de un arreglo son iguales:
+  //retornar true si son todos iguales, caso contrario retornar false.
+  //Escribe tu código aquí
+  
+
+}
+
+function mesesDelAño(array) {
+  //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de
+  //"Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
+  //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+  // Tu código:
+
+}
+
+function elementosMayores(array) {
+  //Realiza una función que retorne la cantidad de los elementos del arreglo donde el valor sea mayor a 18
+  //Escribe tu código aquí:
+
+}
+
+
+function mayorACien(array) {
+  /*   La función recibe como argumento un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
+ valores mayores a 100 (no incluye el 100).
+La funcion debe devolver el nuevo array. */
+  // Tu código:
+
+}
+
+// // No modificar nada debajo de esta línea//
+// // =======================================================================
+module.exports = {
+  sonIguales,
+  tienenMismaLongitud,
+  esPar,
+  esImpar,
+  elevarAlCuadrado,
+  elevarAlCubo,
+  elevar,
+  esPositivo,
+  combinarNombres,
+  obtenerSaludo,
+  agregarSimboloExclamacion,
+  deEuroAdolar,
+  esVocal,
+  esDiezOCinco,
+  estaEnRango,
+  operadoresLogicos,
+  tablaDelSeis,
+  tieneTresDigitos,
+  obtenerLargoDelArray,
+  incrementarPorUno,
+  devolverPrimerElemento,
+  devolverUltimoElemento,
+  dePalabrasAFrase,
+  arrayContiene,
+  agregarNumeros,
+  cuentoElementos,
+  empiezaConNueve,
+  todosIguales,
+  mesesDelAño,
+  elementosMayores,
+  mayorACien,
+};
